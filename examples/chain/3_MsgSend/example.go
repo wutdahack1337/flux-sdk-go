b0e4b039756756704d50fa364ac51b73f6eea4a3
nhannamsiu <dangduynam95@gmail.com>
2023-07-25 09:52:10 +0700

    feat: add proto msg send example

---
+++ examples/chain/3_MsgSend/example.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	chainclient "github.com/FluxNFTLabs/sdk-go/client/chain"
+	"github.com/FluxNFTLabs/sdk-go/client/common"
+	rpchttp "github.com/cometbft/cometbft/rpc/client/http"
+	sdktypes "github.com/cosmos/cosmos-sdk/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+)
+
+func main() {
+	tmClient, err := rpchttp.New("http://localhost:26657", "/websocket")
+	if err != nil {
+		panic(err)
+	}
+
+	senderAddress, cosmosKeyring, err := chainclient.InitCosmosKeyring(
+		os.Getenv("HOME")+"/.fluxd",
+		"fluxd",
+		"file",
+		"user1",
+		"12345678",
+		"88CBEAD91AEE890D27BF06E003ADE3D4E952427E88F88D31D61D3EF5E5D54305",
+		false,
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// initialize grpc client
+	clientCtx, err := chainclient.NewClientContext(
+		"flux-1",
+		senderAddress.String(),
+		cosmosKeyring,
+	)
+	if err != nil {
+		fmt.Println(err)
+	}
+	clientCtx = clientCtx.WithClient(tmClient)
+
+	// prepare tx msg
+	msg := &banktypes.MsgSend{
+		FromAddress: senderAddress.String(),
+		ToAddress:   "lux1jcltmuhplrdcwp7stlr4hlhlhgd4htqhu86cqx",
+		Amount: []sdktypes.Coin{{
+			Denom: "lux", Amount: sdktypes.NewInt(1000000000000000000)}, // 1 LUX
+		},
+	}
+
+	chainClient, err := chainclient.NewChainClient(
+		clientCtx,
+		"localhost:9900",
+		//common.OptionTLSCert(network.ChainTlsCert),
+		common.OptionGasPrices("500000000lux"),
+	)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	//AsyncBroadcastMsg, SyncBroadcastMsg, QueueBroadcastMsg
+	err = chainClient.QueueBroadcastMsg(msg)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	time.Sleep(time.Second * 5)
+
+	gasFee, err := chainClient.GetGasFee()
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("gas fee:", gasFee)
+}
