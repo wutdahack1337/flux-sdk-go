cfeedeb0a292b29934380c9b7b33bdf1f69888b5
nhannamsiu <dangduynam95@gmail.com>
2023-12-13 14:18:17 +0700

    feat: update media stream upload examples with account nonce + header sig

--- examples/indexer/2_UploadStream/audio.go
+++ examples/indexer/2_UploadStream/audio.go
@@ -2,28 +2,44 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
-	types "github.com/FluxNFTLabs/sdk-go/chain/indexer/media"
-	"github.com/goccy/go-json"
+	"github.com/FluxNFTLabs/sdk-go/chain/indexer/account"
+	"github.com/FluxNFTLabs/sdk-go/chain/indexer/media"
+	"github.com/cosmos/cosmos-sdk/crypto/keys/ethsecp256k1"
+	"github.com/cosmos/cosmos-sdk/types/bech32"
+	"github.com/ethereum/go-ethereum/common"
+	ethcrypto "github.com/ethereum/go-ethereum/crypto"
 	ffmpeg "github.com/u2takey/ffmpeg-go"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	grpcmetadata "google.golang.org/grpc/metadata"
 	"math"
 	"os"
 	"strconv"
 )
 
 func main() {
+	senderPrivKey := ethsecp256k1.PrivKey{Key: common.Hex2Bytes("88CBEAD91AEE890D27BF06E003ADE3D4E952427E88F88D31D61D3EF5E5D54305")}
+	senderAddr, _ := bech32.ConvertAndEncode("lux", senderPrivKey.PubKey().Address().Bytes())
+
 	cc, err := grpc.Dial("localhost:4444", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	defer cc.Close()
 	if err != nil {
 		panic(err)
 	}
-	client := types.NewAPIClient(cc)
+	mediaClient := media.NewAPIClient(cc)
 
-	// init upload client
-	uc, err := client.Upload(context.Background())
+	cc, err = grpc.Dial("localhost:4454", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	defer cc.Close()
+	if err != nil {
+		panic(err)
+	}
+	accountClient := account.NewAPIClient(cc)
+
+	ctx := context.Background()
+	acc, err := accountClient.GetAccount(ctx, &account.GetAccountRequest{Address: senderAddr})
 	if err != nil {
 		panic(err)
 	}
@@ -36,10 +52,10 @@ func main() {
 		panic(err)
 	}
 	fmt.Println(chunkCount, chunkTime)
-	metadata := &types.Metadata{
+	metadata := &media.Metadata{
 		Path:       "series_0_1_product" + extension,
 		Encrypted:  false,
-		Type:       types.ContentType_Audio,
+		Type:       media.ContentType_Audio,
 		ChunkCount: chunkCount,
 		ChunkTime:  chunkTime,
 	}
@@ -59,9 +75,35 @@ func main() {
 	fileSize := info.Size()
 	fileIdx := int64(0)
 
+	// prepare header & ctx
+	req := media.StreamMsg{
+		Content: &media.StreamMsg_Metadata{
+			Metadata: metadata,
+		},
+	}
+	nonce := []byte(strconv.FormatUint(acc.Nonce, 10))
+	reqBz, _ := req.Marshal()
+	reqHash := ethcrypto.Keccak256(append(reqBz, nonce...))
+	senderEthPk, _ := ethcrypto.ToECDSA(senderPrivKey.Bytes())
+	reqSig, err := ethcrypto.Sign(reqHash, senderEthPk)
+	if err != nil {
+		panic(err)
+	}
+
+	ctx = grpcmetadata.NewOutgoingContext(ctx, grpcmetadata.MD{
+		"sender":    []string{senderAddr},
+		"signature": []string{common.Bytes2Hex(reqSig)},
+	})
+
+	// init upload client
+	uc, err := mediaClient.Upload(ctx)
+	if err != nil {
+		panic(err)
+	}
+
 	// upload metadata
-	err = uc.Send(&types.StreamMsg{
-		Content: &types.StreamMsg_Metadata{
+	err = uc.Send(&media.StreamMsg{
+		Content: &media.StreamMsg_Metadata{
 			Metadata: metadata,
 		},
 	})
@@ -92,8 +134,8 @@ func main() {
 		fileIdx += int64(idx)
 
 		// upload chunk
-		err = uc.Send(&types.StreamMsg{
-			Content: &types.StreamMsg_MediaChunk{MediaChunk: chunk},
+		err = uc.Send(&media.StreamMsg{
+			Content: &media.StreamMsg_MediaChunk{MediaChunk: chunk},
 		})
 	}
 
--- examples/indexer/2_UploadStream/image.go
+++ examples/indexer/2_UploadStream/image.go
@@ -2,22 +2,39 @@ package main
 
 import (
 	"context"
-	types "github.com/FluxNFTLabs/sdk-go/chain/indexer/media"
+	"github.com/FluxNFTLabs/sdk-go/chain/indexer/account"
+	"github.com/FluxNFTLabs/sdk-go/chain/indexer/media"
+	"github.com/cosmos/cosmos-sdk/crypto/keys/ethsecp256k1"
+	"github.com/cosmos/cosmos-sdk/types/bech32"
+	"github.com/ethereum/go-ethereum/common"
+	ethcrypto "github.com/ethereum/go-ethereum/crypto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	grpcmetadata "google.golang.org/grpc/metadata"
 	"os"
+	"strconv"
 )
 
 func main() {
+	senderPrivKey := ethsecp256k1.PrivKey{Key: common.Hex2Bytes("88CBEAD91AEE890D27BF06E003ADE3D4E952427E88F88D31D61D3EF5E5D54305")}
+	senderAddr, _ := bech32.ConvertAndEncode("lux", senderPrivKey.PubKey().Address().Bytes())
+
 	cc, err := grpc.Dial("localhost:4444", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	defer cc.Close()
 	if err != nil {
 		panic(err)
 	}
-	client := types.NewAPIClient(cc)
+	mediaClient := media.NewAPIClient(cc)
 
-	// init upload client
-	uc, err := client.Upload(context.Background())
+	cc, err = grpc.Dial("localhost:4454", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	defer cc.Close()
+	if err != nil {
+		panic(err)
+	}
+	accountClient := account.NewAPIClient(cc)
+
+	ctx := context.Background()
+	acc, err := accountClient.GetAccount(ctx, &account.GetAccountRequest{Address: senderAddr})
 	if err != nil {
 		panic(err)
 	}
@@ -30,6 +47,11 @@ func main() {
 		panic(err)
 	}
 	defer file.Close()
+	metadata := &media.Metadata{
+		Path:      "series_0_2_product" + extension,
+		Encrypted: false,
+		Type:      media.ContentType_Static,
+	}
 
 	// compute file size
 	info, err := file.Stat()
@@ -39,14 +61,36 @@ func main() {
 	fileSize := info.Size()
 	fileIdx := int64(0)
 
+	// prepare header & ctx
+	req := media.StreamMsg{
+		Content: &media.StreamMsg_Metadata{
+			Metadata: metadata,
+		},
+	}
+	nonce := []byte(strconv.FormatUint(acc.Nonce, 10))
+	reqBz, _ := req.Marshal()
+	reqHash := ethcrypto.Keccak256(append(reqBz, nonce...))
+	senderEthPk, _ := ethcrypto.ToECDSA(senderPrivKey.Bytes())
+	reqSig, err := ethcrypto.Sign(reqHash, senderEthPk)
+	if err != nil {
+		panic(err)
+	}
+
+	ctx = grpcmetadata.NewOutgoingContext(ctx, grpcmetadata.MD{
+		"sender":    []string{senderAddr},
+		"signature": []string{common.Bytes2Hex(reqSig)},
+	})
+
+	// init upload client
+	uc, err := mediaClient.Upload(ctx)
+	if err != nil {
+		panic(err)
+	}
+
 	// upload metadata
-	err = uc.Send(&types.StreamMsg{
-		Content: &types.StreamMsg_Metadata{
-			Metadata: &types.Metadata{
-				Path:      "series_0_2_product" + extension,
-				Encrypted: false,
-				Type:      types.ContentType_Static,
-			},
+	err = uc.Send(&media.StreamMsg{
+		Content: &media.StreamMsg_Metadata{
+			Metadata: metadata,
 		},
 	})
 	if err != nil {
@@ -76,8 +120,8 @@ func main() {
 		fileIdx += int64(idx)
 
 		// upload chunk
-		err = uc.Send(&types.StreamMsg{
-			Content: &types.StreamMsg_MediaChunk{MediaChunk: chunk},
+		err = uc.Send(&media.StreamMsg{
+			Content: &media.StreamMsg_MediaChunk{MediaChunk: chunk},
 		})
 	}
 
