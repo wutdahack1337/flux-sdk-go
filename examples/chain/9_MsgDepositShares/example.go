693be19064dcbec9b390454921d83f0e5f7d269d
nhannamsiu <dangduynam95@gmail.com>
2024-01-23 09:19:34 +0700

    feat: add shares deposit, withdraw, transfer examples

---
+++ examples/chain/9_MsgDepositShares/example.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	sdkmath "cosmossdk.io/math"
+	"fmt"
+	fnfttypes "github.com/FluxNFTLabs/sdk-go/chain/modules/fnft/types"
+	chaintypes "github.com/FluxNFTLabs/sdk-go/chain/types"
+	"github.com/FluxNFTLabs/sdk-go/client/common"
+	"github.com/cosmos/cosmos-sdk/crypto/keyring"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"os"
+	"strings"
+
+	chainclient "github.com/FluxNFTLabs/sdk-go/client/chain"
+)
+
+func main() {
+	network := common.LoadNetwork("local", "")
+	kr, err := keyring.New(
+		"fluxd",
+		"file",
+		os.Getenv("HOME")+"/.fluxd",
+		strings.NewReader("12345678"),
+		chainclient.GetCryptoCodec(),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	// init grpc connection
+	cc, err := grpc.Dial("localhost:9900", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		panic(err)
+	}
+
+	// init client ctx
+	clientCtx, senderAddress, err := chaintypes.NewClientContext(
+		network.ChainId,
+		"user2",
+		kr,
+	)
+	if err != nil {
+		panic(err)
+	}
+	clientCtx = clientCtx.WithGRPCClient(cc)
+
+	// init chain client
+	chainClient, err := chainclient.NewChainClient(
+		clientCtx,
+		common.OptionGasPrices("500000000lux"),
+	)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// prepare tx msg
+	msg := &fnfttypes.MsgDepositShares{
+		Sender:  senderAddress.String(),
+		ClassId: "series",
+		Id:      "0",
+		Shares:  sdkmath.NewIntFromUint64(1750),
+	}
+
+	//AsyncBroadcastMsg, SyncBroadcastMsg, QueueBroadcastMsg
+	err = chainClient.QueueBroadcastMsg(msg)
+	if err != nil {
+		fmt.Println(err)
+	}
+	chainClient.BroadcastDone()
+}
