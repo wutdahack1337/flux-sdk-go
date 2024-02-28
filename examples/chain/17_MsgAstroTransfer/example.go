8ca7390095c5ef307a8671bd5bdf5f7a5e0ad4ad
nhannamsiu <dangduynam95@gmail.com>
2024-02-28 15:30:54 +0700

    feat: add astro transfer examples

---
+++ examples/chain/17_MsgAstroTransfer/example.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"context"
+	"cosmossdk.io/math"
+	"fmt"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"os"
+	"strings"
+
+	astromeshtypes "github.com/FluxNFTLabs/sdk-go/chain/modules/astromesh/types"
+	chaintypes "github.com/FluxNFTLabs/sdk-go/chain/types"
+	"github.com/FluxNFTLabs/sdk-go/client/common"
+	"github.com/cosmos/cosmos-sdk/crypto/keyring"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	chainclient "github.com/FluxNFTLabs/sdk-go/client/chain"
+)
+
+func main() {
+	network := common.LoadNetwork("local", "")
+	kr, err := keyring.New(
+		"fluxd",
+		"file",
+		os.Getenv("HOME")+"/.fluxd",
+		strings.NewReader("12345678\n"),
+		chainclient.GetCryptoCodec(),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	// init grpc connection
+	cc, err := grpc.Dial("localhost:9900", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		panic(err)
+	}
+
+	// init client ctx
+	clientCtx, senderAddress, err := chaintypes.NewClientContext(
+		network.ChainId,
+		"user1",
+		kr,
+	)
+	if err != nil {
+		panic(err)
+	}
+	clientCtx = clientCtx.WithGRPCClient(cc)
+
+	// init chain client
+	chainClient, err := chainclient.NewChainClient(
+		clientCtx,
+		common.OptionGasPrices("500000000lux"),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	// init astromesh query client
+	astromeshClient := astromeshtypes.NewQueryClient(cc)
+
+	// prepare tx msg
+	msg1 := &astromeshtypes.MsgAstroTransfer{
+		Sender:   senderAddress.String(),
+		Receiver: senderAddress.String(),
+		SrcPlane: astromeshtypes.Plane_COSMOS,
+		DstPlane: astromeshtypes.Plane_EVM,
+		Coin: sdk.Coin{
+			Denom:  "lux",
+			Amount: math.NewIntFromUint64(77),
+		},
+	}
+	txResp, err := chainClient.SyncBroadcastMsg(msg1)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("resp:", txResp.TxResponse.TxHash)
+	fmt.Println("gas used/want:", txResp.TxResponse.GasUsed, "/", txResp.TxResponse.GasWanted)
+
+	denomLink, err := astromeshClient.DenomLink(context.Background(), &astromeshtypes.QueryDenomLinkRequest{
+		SrcPlane: astromeshtypes.Plane_COSMOS,
+		DstPlane: astromeshtypes.Plane_EVM,
+		SrcAddr:  "lux",
+	})
+
+	msg2 := &astromeshtypes.MsgAstroTransfer{
+		Sender:   senderAddress.String(),
+		Receiver: senderAddress.String(),
+		SrcPlane: astromeshtypes.Plane_EVM,
+		DstPlane: astromeshtypes.Plane_COSMOS,
+		Coin: sdk.Coin{
+			Denom:  denomLink.DstAddr,
+			Amount: math.NewIntFromUint64(99),
+		},
+	}
+	txResp, err = chainClient.SyncBroadcastMsg(msg2)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("resp:", txResp.TxResponse.TxHash)
+	fmt.Println("gas used/want:", txResp.TxResponse.GasUsed, "/", txResp.TxResponse.GasWanted)
+
+	// to double check locally:
+	// http://localhost:10337/flux/evm/v1beta1/query/{address}/{calldata}
+	// http://localhost:10337/flux/evm/v1beta1/query/a7f16731951d943768cf2053485b69ef61fef8be/aT7IXgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAVvd25lcgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA==
+}
