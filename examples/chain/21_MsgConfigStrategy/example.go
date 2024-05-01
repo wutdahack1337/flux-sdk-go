553eaeead15611f885bfdf35d514e4d3b28fdfd2
nhannamsiu <dangduynam95@gmail.com>
2024-05-01 21:15:35 +0700

    feat: impl strategy config & trigger examples

---
+++ examples/chain/21_MsgConfigStrategy/example.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	strategytypes "github.com/FluxNFTLabs/sdk-go/chain/modules/strategy/types"
+	chaintypes "github.com/FluxNFTLabs/sdk-go/chain/types"
+	chainclient "github.com/FluxNFTLabs/sdk-go/client/chain"
+	"github.com/FluxNFTLabs/sdk-go/client/common"
+	"github.com/cosmos/cosmos-sdk/crypto/keyring"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"os"
+	"strings"
+)
+
+func main() {
+	network := common.LoadNetwork("local", "")
+	kr, err := keyring.New(
+		"fluxd",
+		"file",
+		os.Getenv("HOME")+"/.fluxd",
+		strings.NewReader("12345678"),
+		chainclient.GetCryptoCodec(),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	// init grpc connection
+	cc, err := grpc.Dial("localhost:9900", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		panic(err)
+	}
+
+	// init client ctx
+	clientCtx, senderAddress, err := chaintypes.NewClientContext(
+		network.ChainId,
+		"user1",
+		kr,
+	)
+	if err != nil {
+		panic(err)
+	}
+	clientCtx = clientCtx.WithGRPCClient(cc)
+
+	// init chain client
+	chainClient, err := chainclient.NewChainClient(
+		clientCtx,
+		common.OptionGasPrices("500000000lux"),
+	)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// prepare tx msg
+	dir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	bz, err := os.ReadFile(dir + "/examples/chain/21_MsgConfigStrategy/strategy.wasm")
+	if err != nil {
+		panic(err)
+	}
+	msg := &strategytypes.MsgConfigStrategy{
+		Sender:   senderAddress.String(),
+		Config:   strategytypes.Config_deploy,
+		Id:       "",
+		Strategy: bz,
+	}
+
+	//AsyncBroadcastMsg, SyncBroadcastMsg, QueueBroadcastMsg
+	res, err := chainClient.SyncBroadcastMsg(msg)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(res)
+}
---
+++ examples/chain/22_MsgTriggerStrategy/example.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	strategytypes "github.com/FluxNFTLabs/sdk-go/chain/modules/strategy/types"
+	chaintypes "github.com/FluxNFTLabs/sdk-go/chain/types"
+	chainclient "github.com/FluxNFTLabs/sdk-go/client/chain"
+	"github.com/FluxNFTLabs/sdk-go/client/common"
+	"github.com/cosmos/cosmos-sdk/crypto/keyring"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"os"
+	"strings"
+)
+
+func main() {
+	network := common.LoadNetwork("local", "")
+	kr, err := keyring.New(
+		"fluxd",
+		"file",
+		os.Getenv("HOME")+"/.fluxd",
+		strings.NewReader("12345678"),
+		chainclient.GetCryptoCodec(),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	// init grpc connection
+	cc, err := grpc.Dial("localhost:9900", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		panic(err)
+	}
+
+	// init client ctx
+	clientCtx, senderAddress, err := chaintypes.NewClientContext(
+		network.ChainId,
+		"user1",
+		kr,
+	)
+	if err != nil {
+		panic(err)
+	}
+	clientCtx = clientCtx.WithGRPCClient(cc)
+
+	// init chain client
+	chainClient, err := chainclient.NewChainClient(
+		clientCtx,
+		common.OptionGasPrices("500000000lux"),
+	)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// prepare tx msg
+	if err != nil {
+		panic(err)
+	}
+	msg := &strategytypes.MsgTriggerStrategies{
+		Sender: senderAddress.String(),
+		Ids:    []string{"99e3cf15446aa88463aa9306fb2e2eb2076c4f792cdfed847392f05ab6c975c4"},
+		Inputs: [][]byte{{0}},
+	}
+
+	//AsyncBroadcastMsg, SyncBroadcastMsg, QueueBroadcastMsg
+	res, err := chainClient.SyncBroadcastMsg(msg)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(res)
+}
