9f3b0505a89be49950068551ac0732d40a9c095c
nhannamsiu <dangduynam95@gmail.com>
2024-05-30 09:18:30 +0700

    feat: add example to deploy uniswap to evm plane

---
+++ examples/chain/28_DeployUniswap/example.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"math/big"
+	"os"
+	"strings"
+
+	evmtypes "github.com/FluxNFTLabs/sdk-go/chain/modules/evm/types"
+	chaintypes "github.com/FluxNFTLabs/sdk-go/chain/types"
+	"github.com/FluxNFTLabs/sdk-go/client/common"
+	"github.com/cosmos/cosmos-sdk/crypto/keyring"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	chainclient "github.com/FluxNFTLabs/sdk-go/client/chain"
+)
+
+func main() {
+	network := common.LoadNetwork("local", "")
+	kr, err := keyring.New(
+		"fluxd",
+		"file",
+		os.Getenv("HOME")+"/.fluxd",
+		strings.NewReader("12345678\n"),
+		chainclient.GetCryptoCodec(),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	// init grpc connection
+	cc, err := grpc.Dial("localhost:9900", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		panic(err)
+	}
+
+	// init client ctx
+	clientCtx, senderAddress, err := chaintypes.NewClientContext(
+		network.ChainId,
+		"user1",
+		kr,
+	)
+	if err != nil {
+		panic(err)
+	}
+	clientCtx = clientCtx.WithGRPCClient(cc)
+
+	// init chain client
+	chainClient, err := chainclient.NewChainClient(
+		clientCtx,
+		common.OptionGasPrices("500000000lux"),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	// read bytecode
+	dir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	bz, err := os.ReadFile(dir + "/examples/chain/28_DeployUniswap/PoolManager.json")
+	if err != nil {
+		panic(err)
+	}
+
+	var compData map[string]interface{}
+	err = json.Unmarshal(bz, &compData)
+	if err != nil {
+		panic(err)
+	}
+	bytecode, err := hex.DecodeString(compData["bytecode"].(map[string]interface{})["object"].(string))
+	if err != nil {
+		panic(err)
+	}
+	abiBz, err := json.Marshal(compData["abi"].([]interface{}))
+	if err != nil {
+		panic(err)
+	}
+	contractABI, err := abi.JSON(strings.NewReader(string(abiBz)))
+	if err != nil {
+		panic(err)
+	}
+
+	// pack constructor data
+	controllerGasLimit, err := contractABI.Pack("", big.NewInt(4000000))
+	if err != nil {
+		panic(err)
+	}
+
+	// prepare tx msg
+	msg := &evmtypes.MsgDeployContract{
+		Sender:   senderAddress.String(),
+		Bytecode: bytecode,
+		Calldata: controllerGasLimit,
+	}
+
+	txResp, err := chainClient.SyncBroadcastMsg(msg)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("tx hash:", txResp.TxResponse.TxHash)
+	fmt.Println("gas used/want:", txResp.TxResponse.GasUsed, "/", txResp.TxResponse.GasWanted)
+
+	hexResp, err := hex.DecodeString(txResp.TxResponse.Data)
+	if err != nil {
+		panic(err)
+	}
+
+	// decode result to get contract address
+	var txData sdk.TxMsgData
+	if err := txData.Unmarshal(hexResp); err != nil {
+		panic(err)
+	}
+
+	var dcr evmtypes.MsgDeployContractResponse
+	if err := dcr.Unmarshal(txData.MsgResponses[0].Value); err != nil {
+		panic(err)
+	}
+	fmt.Println("contract address:", hex.EncodeToString(dcr.ContractAddress))
+}
