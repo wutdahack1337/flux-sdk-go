f9a6f9238dfde92a6ee1902b9b933e37d0b0e998
nhannamsiu <dangduynam95@gmail.com>
2024-06-10 20:31:02 +0700

    feat: add config cron example

---
+++ examples/chain/26_MsgConfigCron/example.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"cosmossdk.io/math"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/FluxNFTLabs/sdk-go/chain/modules/astromesh/types"
+	strategytypes "github.com/FluxNFTLabs/sdk-go/chain/modules/strategy/types"
+	chaintypes "github.com/FluxNFTLabs/sdk-go/chain/types"
+	chainclient "github.com/FluxNFTLabs/sdk-go/client/chain"
+	"github.com/FluxNFTLabs/sdk-go/client/common"
+	"github.com/cosmos/cosmos-sdk/crypto/keyring"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	network := common.LoadNetwork("local", "")
+	kr, err := keyring.New(
+		"fluxd",
+		"file",
+		os.Getenv("HOME")+"/.fluxd",
+		strings.NewReader("12345678"),
+		chainclient.GetCryptoCodec(),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	// init grpc connection
+	cc, err := grpc.Dial("localhost:9900", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		panic(err)
+	}
+
+	// init client ctx
+	clientCtx, senderAddress, err := chaintypes.NewClientContext(
+		network.ChainId,
+		"user1",
+		kr,
+	)
+	if err != nil {
+		panic(err)
+	}
+	clientCtx = clientCtx.WithGRPCClient(cc)
+
+	// init chain client
+	chainClient, err := chainclient.NewChainClient(
+		clientCtx,
+		common.OptionGasPrices("500000000lux"),
+	)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// prepare tx msg
+	dir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	bz, err := os.ReadFile(dir + "/examples/chain/26_MsgConfigCron/cron.wasm")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("sender: ", senderAddress.String())
+	msg := &strategytypes.MsgConfigStrategy{
+		Sender:   senderAddress.String(),
+		Config:   strategytypes.Config_deploy,
+		Id:       "",
+		Strategy: bz,
+		Query: &types.FISQueryRequest{
+			// track balances of accounts you want to make the amount even
+			Instructions: []*types.FISQueryInstruction{},
+		},
+		TriggerPermission: &strategytypes.PermissionConfig{
+			Type:      strategytypes.AccessType_only_addresses,
+			Addresses: []string{senderAddress.String()},
+		},
+		Metadata: &strategytypes.StrategyMetadata{
+			Name:        "bank cron",
+			Description: "transfer 1 usdt to account lux158ucxjzr6ccrlpmz8z05wylu8tr5eueqcp2afu every block",
+			Logo:        "",
+			Website:     "",
+			Type:        strategytypes.StrategyType_CRON,
+			Tags:        []string{"cron", "bank", "util"},
+			Schema:      "",
+			GasPrice:    math.NewIntFromUint64(500000000),
+		},
+	}
+
+	//AsyncBroadcastMsg, SyncBroadcastMsg, QueueBroadcastMsg
+	res, err := chainClient.SyncBroadcastMsg(msg)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("gas used/want:", res.TxResponse.GasUsed, "/", res.TxResponse.GasWanted)
+}
