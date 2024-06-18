429bc4390f56fa8d2056401261debc913dc69af2
nhannamsiu <dangduynam95@gmail.com>
2024-06-18 21:09:36 +0700

    feat: udpate fis query example

--- examples/chain/19_FISBankBalanceQuery/example.go
+++ examples/chain/19_FISBankBalanceQuery/example.go
@@ -7,7 +7,6 @@ import (
 	chaintypes "github.com/FluxNFTLabs/sdk-go/chain/types"
 	"github.com/FluxNFTLabs/sdk-go/client/common"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
@@ -40,19 +39,13 @@ func main() {
 	astromeshClient := astromeshtypes.NewQueryClient(cc)
 
 	// query
-	path := "/cosmos.bank.v1beta1.Query/AllBalances"
-	req := &banktypes.QueryAllBalancesRequest{
-		Address: "lux1jcltmuhplrdcwp7stlr4hlhlhgd4htqhu86cqx",
-	}
-	reqBz, _ := clientCtx.Codec.Marshal(req)
 	fisReq := &astromeshtypes.FISQueryRequest{Instructions: []*astromeshtypes.FISQueryInstruction{
 		{
 			Plane:   astromeshtypes.Plane_COSMOS,
 			Action:  astromeshtypes.QueryAction_COSMOS_QUERY,
 			Address: []byte{},
 			Input: [][]byte{
-				[]byte(path),
-				reqBz,
+				[]byte("/cosmos/bank/v1beta1/balances/lux1jcltmuhplrdcwp7stlr4hlhlhgd4htqhu86cqx"),
 			},
 		},
 	}}
