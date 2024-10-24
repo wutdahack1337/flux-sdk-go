78b5f60e0b7fade43d23058a1f8d8e1d26f32d76
Minh-Nguyen521 <minhb2306674@student.ctu.edu.vn>
2024-10-24 19:25:38 +0700

    feat: add trigger

---
+++ examples/chain/45_TriggerDriftSolver/example.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+
+	"cosmossdk.io/math"
+	astromeshtypes "github.com/FluxNFTLabs/sdk-go/chain/modules/astromesh/types"
+	strategytypes "github.com/FluxNFTLabs/sdk-go/chain/modules/strategy/types"
+	chaintypes "github.com/FluxNFTLabs/sdk-go/chain/types"
+	chainclient "github.com/FluxNFTLabs/sdk-go/client/chain"
+	"github.com/FluxNFTLabs/sdk-go/client/common"
+	"github.com/cosmos/btcutil/base58"
+	"github.com/cosmos/cosmos-sdk/crypto/keyring"
+	"github.com/cosmos/cosmos-sdk/crypto/keys/ed25519"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	network := common.LoadNetwork("local", "")
+	kr, err := keyring.New(
+		"fluxd",
+		"file",
+		os.Getenv("HOME")+"/.fluxd",
+		strings.NewReader("12345678"),
+		chainclient.GetCryptoCodec(),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	// init grpc connection
+	cc, err := grpc.Dial(network.ChainGrpcEndpoint, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		panic(err)
+	}
+
+	// init client ctx
+	clientCtx, senderAddress, err := chaintypes.NewClientContext(
+		network.ChainId,
+		"user2",
+		kr,
+	)
+	if err != nil {
+		panic(err)
+	}
+	clientCtx = clientCtx.WithGRPCClient(cc)
+
+	// init chain client
+	chainClient, err := chainclient.NewChainClient(
+		clientCtx,
+		common.OptionGasPrices("500000000lux"),
+	)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// prepare tx msg
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("sender address:", senderAddress.String())
+	
+	// check if account is linked, not then create
+	isSvmLinked, svmPubkey, err := chainClient.GetSVMAccountLink(context.Background(), senderAddress)
+	if err != nil {
+		panic(err)
+	}
+
+	if !isSvmLinked {
+		svmKey := ed25519.GenPrivKey() // Good practice: Backup this private key
+		res, err := chainClient.LinkSVMAccount(svmKey, math.NewIntFromUint64(1000_000_000_000))
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("linked sender to svm address:", base58.Encode(svmKey.PubKey().Bytes()), "txHash:", res.TxResponse.TxHash)
+	} else {
+		fmt.Println("sender is already linked to svm address:", svmPubkey.String())
+	}
+
+
+	msgTriggerStategy := &strategytypes.MsgTriggerStrategies{
+		Sender: senderAddress.String(),
+		Ids:    []string{"B9D706AC02506DD2369C82EA0BC81C197FDCB0B2B28E29EF88097B923863E68D"},
+		Inputs: [][]byte{
+			[]byte(`{"place_perp_market_order":{"market":"btc","usdt_amount":"50","leverage":"3","auction_duration":"10"}}`),
+		},
+		Queries: []*astromeshtypes.FISQueryRequest{
+			{
+				Instructions: []*astromeshtypes.FISQueryInstruction{
+					{
+						Plane:   astromeshtypes.Plane_COSMOS,
+						Action:  astromeshtypes.QueryAction_COSMOS_BANK_BALANCE,
+						Address: nil,
+						Input: [][]byte{
+							[]byte(svmPubkey.String()),
+						},
+					},
+				},
+			},
+		},
+	}
+	res, err := chainClient.SyncBroadcastMsg(msgTriggerStategy)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("tx hash:", res.TxResponse.TxHash)
+	fmt.Println("gas used/want:", res.TxResponse.GasUsed, "/", res.TxResponse.GasWanted)
+}
