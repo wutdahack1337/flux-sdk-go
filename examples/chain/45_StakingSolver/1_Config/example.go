aab94bbe3d4a17fdb16dedc29745d322fe7a0765
Minh-Nguyen521 <minhb2306674@student.ctu.edu.vn>
2024-11-01 00:17:07 +0700

    feat: update description

--- examples/chain/45_StakingSolver/1_Config/example.go
+++ examples/chain/45_StakingSolver/1_Config/example.go
@@ -75,7 +75,7 @@ func main() {
 		Query:    &types.FISQueryRequest{},
 		Metadata: &strategytypes.StrategyMetadata{
 			Name:        "Staking Solver",
-			Description: "The staking solver on Astromesh streamlines the staking process, helping users delegate tokens securely and optimize rewards",
+			Description: "The staking solver on Astromesh streamlines the staking process, helping users delegate tokens securely and optimize rewards\n\n**Available options:**\n\nValidator_name: `flux`",
 			Logo:        "https://icons.veryicon.com/png/o/business/work-circle/proof-of-stake.png",
 			Website:     "https://www.astromesh.xyz",
 			Type:        strategytypes.StrategyType_INTENT_SOLVER,
